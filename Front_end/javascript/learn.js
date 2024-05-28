c60e0bcd6a35c2e572b336021c3535be411435ff
Ays <minigun5@naver.com>
2024-05-28 23:47:47 +0900

    [Add] 생성자에 대한 학습 내용 추가

--- Front_end/javascript/learn.js
+++ Front_end/javascript/learn.js
@@ -58,3 +58,44 @@
 //         console.log(Player.hp); // Player의 hp 프로퍼티 참조
 //     }
 // }
+
+// // 생성자
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+
+//     this.introduce = function () {
+//         console.log(`안녕하세요, 저는 ${this.name}입니다. ${this.age}살입니다.`);
+//     };
+// }
+
+// // 생성자로 두 객체 생성
+// const person1 = new Person('김기태', 28);
+// const person2 = new Person('김성표', 24);
+
+// // 생성자를 통해 생성된 두 객체는 같은 프로퍼티를 공유한다.
+// console.log(person1); // Person {name: '김기태', age: 28, introduce: ƒ}
+// console.log(person2); // Person {name: '김성표', age: 24, introduce: ƒ}
+
+// // 마찬가지로 같은 메소드를 공유한다.
+// console.log(person1.introduce()); // 안녕하세요, 저는 김기태입니다. 28살입니다.
+// console.log(person2.introduce()); //안녕하세요, 저는 김성표입니다. 24살입니다.
+
+// function Factory() { } // 생성자 정의
+// let instance = new Factory(); // 객체 생성
+// instance instanceof Factory // 생성자와 객체의 관계 확인: true 출력
+
+// // 생성자
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.introduce = function () {
+//         console.log(`반갑습니다. 저의 이름은 ${this.name}이고, 나이는 ${this.age}살입니다.`);
+//     };
+// }
+
+// const man = new Person('서진태', 26); // 생성자로 객체(인스턴스) 생성
+
+// console.log(man.name); // 서진태
+// console.log(man.age); // 26
+// console.log(man.introduce()); // 반갑습니다. 저의 이름은 서진태이고, 나이는 26살입니다.
